7a73725f02dd7ab5e4314c7c22b526cbb030f679
Ellie <eagyeman1234@gmail.com>
2023-10-07 19:54:07 -0400

    import state and getAllStarShips

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+import { getAllStarShips } from '../services/sw-api'
+import './App.css'
+
+function App() {
+  const fetchStarShips = async () = useState([])
+  
+  return (
+    <>
+
+    
+    </>
+  )
+}
+
+export default App
